3d2777266c8fadf5b2d38c4940f44d8e8292e48a
Shania Roy <shro5435@colorado.edu>
2018-09-26 22:37:07 -0600

    my commit

---
+++ Animal.h
@@ -0,0 +1,39 @@
+#ifndef _ANIMAL_H_
+#define _ANIMAL_H_
+
+class Animal {
+public:
+	Animal(std::string sound):
+	sound_(sound)
+	{}
+
+	std::string MakeSound() const {return sound_; }
+
+	void Fight(Animal &opponent);
+
+	virtual int GetPower() { return 0; };
+
+private:
+	std::string sound_;
+};
+
+class Turtle : public Animal {
+public:
+	Turtle():
+	Animal("turtle turtle turtle")
+	int GetPower() { return 7; };
+
+	{}
+
+class Pig(): public Animal 
+	Animal ("Oink Oink")
+	{}
+	int GetPower(){return 10;}; 
+	{}
+
+
+};
+
+// Define your animals here
+
+#endif  // _ANIMAL_H_
--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+#include "Animal.h"
+
+int main() {
+	Turtle turtle_friend;
+
+	std::cout << turtle_friend.MakeSound() << std::endl;
+
+	Pig Pig_friend; 
+	std:: cout<< Pig_friend.MakeSound() << std::endl; 
+
+	return 0;
+}
